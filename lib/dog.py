e6f6a85125f971ed30a055a9606985b1694690f7
JOY19ANYANGO <joyanyango402@gmail.com>
2023-08-20 22:02:10 +0300

    done

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
 
 class Dog:
-    # Class body goes here
+    def bark(self):
+        print("Woof!")
+        
+    def sit(self):
+        print("The dog is sitting.")
 
-    #Instance method definition
-    pass
+fido = Dog()
+fido.bark()
+fido.sit() 
+# Woof!
+# The dog is sitting.
+snoopy = Dog()
+snoopy.bark() 
+snoopy.bark()
+# Woof!
+# The dog is sitting.
